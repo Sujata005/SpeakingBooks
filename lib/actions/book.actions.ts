c18f65ef5b52c2c49b4569ae6f817b788ec50807
Sujata <112793420+Sujata005@users.noreply.github.com>
2026-04-13 16:36:08 +0530

    fix: access provate->public

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -17,7 +17,7 @@ export const uploadToBlob = async (formData: FormData) => {
 
   // This runs on the server, so it has access to BLOB_READ_WRITE_TOKEN
   const blob = await put(filename, file, {
-    access: 'private',
+    access: 'public',
     addRandomSuffix: true,
   });
 
